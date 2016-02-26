e19c6241f199188dd89632939848edd735120987
Matthew Taylor <matt@numenta.org>
2016-02-26 11:08:26 -0800

    Updated code and comments.

--- collect_samples.py
+++ collect_samples.py
@@ -1,4 +1,5 @@
 import os
+import platform
 import psutil
 from datetime import datetime
 import time
@@ -14,12 +15,15 @@ USERNAME = None
 PASSWORD = None
 USE_SSL = False
 
-USER = os.environ["USER"]
 SAMPLE_COUNT = 100
-SAMPLE_INTERVAL_SECONDS = 1
+SAMPLE_INTERVAL_SECONDS = 0.5
 
 
 def main():
+  """
+  Main demo program. Takes samples of computer metrics and stores them into
+  InfluxDB.
+  """
   influxClient = InfluxDBClient(
     host=HOST,
     port=PORT,
@@ -30,31 +34,40 @@ def main():
 
   setupDatabase(influxClient)
 
-  timezone = datetime.now(tzlocal()).tzname()
-
   for i in xrange(SAMPLE_COUNT):
     print "Collection sample of your computer metrics..."
     if i % 10 == 0:
       print "\t({} samples remaining before I quit)".format(SAMPLE_COUNT - i)
+
+    # Sample computer for metrics.
     sample = getSample()
-    timestamp = int(time.mktime(sample["time"].timetuple())) * 1000000000
 
-    payload = createInfluxPayload(sample, timestamp, timezone)
+    # Create an object to write to InfluxDB containing all sample measurements.
+    payload = createInfluxPayload(sample)
 
-    # Write all measurements
+    # Write all measurements.
     influxClient.write_points(payload)
+
     # Wait.
     time.sleep(SAMPLE_INTERVAL_SECONDS)
 
 
-def createInfluxPayload(sample, timestamp, timezone):
+def createInfluxPayload(sample):
+  """
+  Given a metric sample, creates a payload object to write to InfluxDB.
+  """
   payload = []
+  sampleTime = sample["time"]
+  # This timestamp needs to be 19 digits long and contain millisecond
+  # precision.
+  timestamp = int(
+    time.mktime(sampleTime.timetuple())*1e3 + sampleTime.microsecond/1e3
+  ) * 1000000
+
+
   # CPU measurement
   payload.append({
-    "tags": {
-      "user": USER,
-      "timezone": timezone,
-    },
+    "tags": getTags(),
     "time": timestamp,
     "measurement": "cpu",
     "fields": {
@@ -63,10 +76,7 @@ def createInfluxPayload(sample, timestamp, timezone):
   })
   # Disk measurement
   payload.append({
-    "tags": {
-      "user": USER,
-      "timezone": timezone,
-    },
+    "tags": getTags(),
     "time": timestamp,
     "measurement": "disk",
     "fields": {
@@ -77,10 +87,7 @@ def createInfluxPayload(sample, timestamp, timezone):
   })
   # Memory measurement
   payload.append({
-    "tags": {
-      "user": USER,
-      "timezone": timezone,
-    },
+    "tags": getTags(),
     "time": timestamp,
     "measurement": "memory",
     "fields": {
@@ -89,10 +96,7 @@ def createInfluxPayload(sample, timestamp, timezone):
   })
   # Network measurement
   payload.append({
-    "tags": {
-      "user": USER,
-      "timezone": timezone,
-    },
+    "tags": getTags(),
     "time": timestamp,
     "measurement": "network",
     "fields": {
@@ -103,7 +107,23 @@ def createInfluxPayload(sample, timestamp, timezone):
   return payload
 
 
+def getTags():
+  """
+  Gets the InfluxDB tags we want to use for metrics being stored from this
+  program.
+  """
+  return {
+    "user": os.environ["USER"],
+    # "host": platform.node(),
+    "timezone": datetime.now(tzlocal()).tzname(),
+  }
+
+
 def getSample():
+  """
+  Samples some metrics on this computer right now. Returns timestamp of the
+  sample with the sample.
+  """
   diskCounters = psutil.disk_io_counters()
   netCounters = psutil.net_io_counters()
   return {
@@ -119,12 +139,30 @@ def getSample():
 
 
 def setupDatabase(client):
+  """
+  Creates a database for this demo if one does not exist. Creates a 1-hour
+  retention policy if one does not exist. Switches to use the new database for
+  default queries.
+  """
   databases = client.get_list_database()
   if DB not in [d["name"] for d in databases]:
     print "Creating Influx database '%s'..." % DB
     client.create_database(DB)
+
   print "Using Influx database '%s'." % DB
   client.switch_database(DB)
+
+  retentionPolicyName = "one_hour"
+  retentionPolicyDuration = "1h"
+  retentionPolicies = client.get_list_retention_policies()
+  if retentionPolicyName not in [rp["name"] for rp in retentionPolicies]:
+    print "Creating {} retention policy called \"{}\"...".format(
+      retentionPolicyDuration, retentionPolicyName
+    )
+    client.create_retention_policy(
+      retentionPolicyName, retentionPolicyDuration, 1, database=DB, default=True
+    )
+
 
 
 if __name__ == "__main__":
